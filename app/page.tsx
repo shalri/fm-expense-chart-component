7f85f71758934a1bf0da4259c04c3b6a33d79a76
shalri <fsaonlinework777@gmail.com>
2024-10-23 14:25:41 +0800

    Init

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+import SandBox from "../components/Sandbox";
+import Footer from "../components/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <SandBox />
+      <Footer />
+    </>
+  );
+}
---
+++ components/Sandbox.tsx
@@ -0,0 +1,11 @@
+export default function SandBox() {
+  return (
+    <main className="flex flex-grow flex-col items-center justify-center p-24">
+      <div className="flex flex-col items-center justify-center text-center">
+        <h1 className="text-3xl font-bold text-zinc-500">
+          a NextJS base template
+        </h1>
+      </div>
+    </main>
+  );
+}
